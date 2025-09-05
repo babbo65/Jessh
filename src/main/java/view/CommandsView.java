528fff0c906f3c0455524c849e00e377f283631e
Brian Abbott <brianabbott214@gmail.com>
2025-09-04 22:18:53 -0400

    Gui works now

---
+++ src/main/java/view/CommandsView.java
@@ -0,0 +1,21 @@
+package view;
+
+import controller.Connect;
+import model.Connection;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class CommandsView {
+    public ErrorInfo sendAndReceive(Connection connection)
+    {
+
+        return null;
+    }
+
+}
+class ErrorInfo{
+    public String host;
+    public String shortErrorMessage;
+    public String fullError;
+}
